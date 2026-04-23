5b2d5b72516c675673ee93bec8b9d8a817887585
KimYongHun27 <ryh9911@gmail.com>
2026-04-23 16:29:31 +0900

    startSpringBoot

---
+++ src/test/java/com/meta12/infoArchive/InfoArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta12.infoArchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
