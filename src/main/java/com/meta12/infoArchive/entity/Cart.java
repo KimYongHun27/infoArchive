04edcfbe031c4e5c98aaa8f8886505e81ce74152
KimYongHun27 <ryh9911@gmail.com>
2026-04-27 14:07:24 +0900

    update_YongHun

---
+++ src/main/java/com/meta12/infoArchive/entity/Cart.java
@@ -0,0 +1,15 @@
+package com.meta12.infoArchive.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Cart {
+    /*
+
+
+    */
+}
---
+++ src/main/java/com/meta12/infoArchive/repository/WishListRepository.java
@@ -0,0 +1,9 @@
+package com.meta12.infoArchive.repository;
+
+import com.meta12.infoArchive.entity.WishList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WishListRepository extends JpaRepository<WishList, Long> {
+}
