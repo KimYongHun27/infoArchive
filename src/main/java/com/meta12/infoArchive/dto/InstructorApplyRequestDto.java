2dde95448be2e27fcc2485ad265ee0014da85aa1
mate12 <cnjzzan@naver.com>
2026-04-27 16:55:08 +0900

    Dto

--- src/main/java/com/meta12/infoArchive/dto/InstructorApplyRequestDto.java
+++ src/main/java/com/meta12/infoArchive/dto/InstructorApplyRequestDto.java
@@ -7,9 +7,9 @@ import lombok.Setter;
 @Setter
 public class InstructorApplyRequestDto {
 
-    private Long userId;
-    private String title;
-    private String intro;
-    private String career;
-    private String portfolioUrl;
+    private Long userId;                    // 신청자 회원 ID
+    private String applicationTitle;        // 강사 신청 제목
+    private String instructorIntro;         // 강사 소개
+    private String instructorCareer;        // 강사 경력
+    private String portfolioUrl;            // 포트폴리오 주소
 }
