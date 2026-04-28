8ea75ba990143c9c41fa21ce09f7e90bb7551565
ABC-0099 <seonghoi347@gmail.com>
2026-04-28 11:21:43 +0900

    SungHo

---
+++ src/main/java/com/meta12/infoArchive/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.meta12.infoArchive.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/main")
+    public String list(){
+        return "main";
+    }
+}
