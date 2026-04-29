1d461d26f5625e977ce58503375f2c0ecd5a2eb5
ABC-0099 <seonghoi347@gmail.com>
2026-04-29 15:46:06 +0900

    Sungho

--- src/main/java/com/meta12/infoArchive/dto/ReviewDto.java
+++ src/main/java/com/meta12/infoArchive/dto/ReviewDto.java
@@ -2,6 +2,7 @@ package com.meta12.infoArchive.dto;
 
 import com.meta12.infoArchive.entity.Instructor;
 import com.meta12.infoArchive.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,13 +15,16 @@ public class ReviewDto {
 
     private Long id;
 
+    private  String title;
+
     // 내용
     private String content;
 
     // 작성자
-    private User author;
+    private User nameid;
+    //id
     // 강사 작성자 연동
-    private Instructor nickname;
+    private Instructor nicknameid;
 
     // 날짜 생성
     private LocalDateTime createDate;
