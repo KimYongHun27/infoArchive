ef5444367c9ba1bb807adc17361274c4ecc903f4
KimYongHun27 <ryh9911@gmail.com>
2026-05-11 09:05:47 +0900

    KimYongHun

--- src/main/java/com/meta12/infoArchive/InfoArchiveApplication.java
+++ src/main/java/com/meta12/infoArchive/InfoArchiveApplication.java
@@ -3,6 +3,7 @@ package com.meta12.infoArchive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class InfoArchiveApplication {
 
