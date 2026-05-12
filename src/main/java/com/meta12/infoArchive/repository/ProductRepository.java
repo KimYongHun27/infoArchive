653bac62769ab26f982e831ebe2cf573ae815501
mate12 <cnjzzan@naver.com>
2026-05-12 12:37:38 +0900

    Jeong Won

--- src/main/java/com/meta12/infoArchive/repository/ProductRepository.java
+++ src/main/java/com/meta12/infoArchive/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.meta12.infoArchive.repository;
 import com.meta12.infoArchive.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByTitleContaining(String keyword);
 }
