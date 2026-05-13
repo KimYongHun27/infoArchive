8b70607d226abc7c91c38c53b053cab63c44bcbf
mate12 <cnjzzan@naver.com>
2026-05-13 11:43:48 +0900

    Jeong Won

--- src/main/java/com/meta12/infoArchive/controller/ProductController.java
+++ src/main/java/com/meta12/infoArchive/controller/ProductController.java
@@ -1,7 +1,19 @@
 package com.meta12.infoArchive.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
+
+    @GetMapping("/product/{id}")
+    public String productDetail(
+            @PathVariable Long id,
+            Model model
+    ) {
+        model.addAttribute("productId", id);
+        return "product/detail";
+    }
 }
