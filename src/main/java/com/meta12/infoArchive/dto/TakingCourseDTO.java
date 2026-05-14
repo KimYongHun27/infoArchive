a45258f6507c65e3f50d40fa88591f6705eeaa1a
류민혜 <peinture8@naver.com>
2026-05-14 12:05:04 +0900

    ryuminhae
    ryuminhae

--- src/main/java/com/meta12/infoArchive/dto/TakingCourseDTO.java
+++ src/main/java/com/meta12/infoArchive/dto/TakingCourseDTO.java
@@ -1,4 +1,14 @@
 package com.meta12.infoArchive.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TakingCourseDTO {
+    private String title;
+    private String instructor;
+    private String category;
+    private Integer progressRate;
+    private String thumbnailUrl;
 }
--- src/main/java/com/meta12/infoArchive/entity/TakingCourse.java
+++ src/main/java/com/meta12/infoArchive/entity/TakingCourse.java
@@ -17,16 +17,17 @@ public class TakingCourse {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;                 // 강의 고유 번호
 
-    private String name;             // 강의 이름
+    private String title;             // 강의 이름
     private String instructor;       // 강사 이름
-    private String intro;            // 강의 소개
     private String category;         // 강의 분야
 
     @OneToOne
     @JoinColumn(name = "user_id")
-    private User user;               // 연결된 회원 정보
+    private User user;                 // 연결된 회원 정보
 
-    private LocalDateTime createdAt; // 강의 등록일
+    private Integer progressRate = 0; // 학습 진도율 (0 ~ 100%)
+
+    private LocalDateTime createdAt;  // 강의 등록일
 
     @PrePersist
     public void prePersist() {
