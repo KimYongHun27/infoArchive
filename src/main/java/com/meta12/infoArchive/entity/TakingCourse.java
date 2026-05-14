a684b90390d56aa3e8a8e08debd58a24d7267701
류민혜 <peinture8@naver.com>
2026-05-14 11:43:50 +0900

    Update TakingCourse.java

--- src/main/java/com/meta12/infoArchive/entity/TakingCourse.java
+++ src/main/java/com/meta12/infoArchive/entity/TakingCourse.java
@@ -1,40 +1,35 @@
 package com.meta12.infoArchive.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TakingCourse {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;                 // 강의 고유 번호
 
-    //유저 fk
-    @ManyToOne(fetch = FetchType.LAZY)
+    private String name;             // 강의 이름
+    private String instructor;       // 강사 이름
+    private String intro;            // 강의 소개
+    private String category;         // 강의 분야
+
+    @OneToOne
     @JoinColumn(name = "user_id")
-    private User user;
+    private User user;               // 연결된 회원 정보
 
-    //강의 fk
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "lecture_id")
-    private Lecture lecture;
+    private LocalDateTime createdAt; // 강의 등록일
 
-    //최근 배운 순서 정렬용
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
-
-    public static TakingCourse createTakingCourse(User user, Lecture lecture) {
-        TakingCourse takingCourse = new TakingCourse();
-        takingCourse.user = user;
-        takingCourse.lecture = lecture;
-        return takingCourse;
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
     }
 }
