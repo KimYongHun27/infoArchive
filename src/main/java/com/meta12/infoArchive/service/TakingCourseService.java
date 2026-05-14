cba22c41f1710b680eddd7dbf6fc1a2cead283d1
류민혜 <peinture8@naver.com>
2026-05-14 12:46:37 +0900

    ryuminhae
    ryuminhae

---
+++ src/main/java/com/meta12/infoArchive/service/TakingCourseService.java
@@ -0,0 +1,9 @@
+package com.meta12.infoArchive.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TakingCourseService {
+}
