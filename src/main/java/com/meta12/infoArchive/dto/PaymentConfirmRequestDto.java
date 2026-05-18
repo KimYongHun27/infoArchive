26d42978ddc917deafdabb914ca771688b7b2e96
mate12 <cnjzzan@naver.com>
2026-05-18 12:29:16 +0900

    Jeong Won

--- src/main/java/com/meta12/infoArchive/dto/PaymentConfirmRequestDto.java
+++ src/main/java/com/meta12/infoArchive/dto/PaymentConfirmRequestDto.java
@@ -13,10 +13,12 @@ public class PaymentConfirmRequestDto {
     private Integer amount;
     private String paymentMethod;
     private String membershipType;
+    private Boolean agreeTerms;
 
     // MOCK 카드 정보
     private String cardNumber;
     private String cardExpire;
     private String cardCvc;
     private String cardPassword;
+
 }
