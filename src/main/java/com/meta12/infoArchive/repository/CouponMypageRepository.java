fe43999a05cf0141c1338927da7ce10656cdf325
류민혜 <peinture8@naver.com>
2026-05-18 15:11:46 +0900

    ryuminhae
    ryuminhae

--- src/main/java/com/meta12/infoArchive/repository/CouponMypageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meta12.infoArchive.repository;
-
-import com.meta12.infoArchive.entity.CouponMypage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CouponMypageRepository extends JpaRepository<CouponMypage, Long> {
-}
