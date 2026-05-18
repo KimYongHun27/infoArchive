b171087c186aa6fa33184ff9c26116cee4da2845
ABC-0099 <seonghoi347@gmail.com>
2026-05-18 11:54:41 +0900

    Sungho

--- src/main/java/com/meta12/infoArchive/controller/CommunityController.java
+++ src/main/java/com/meta12/infoArchive/controller/CommunityController.java
@@ -1,13 +1,17 @@
 package com.meta12.infoArchive.controller;
 
+import com.meta12.infoArchive.dto.CommunityDto;
+import com.meta12.infoArchive.entity.Community;
 import com.meta12.infoArchive.entity.Review;
 import com.meta12.infoArchive.service.CommunityService;
 import com.meta12.infoArchive.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +19,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommunityController {
 
+    private final CommunityService communityService;
+
     @GetMapping("/community")
-      public String community()
+      public String community(
+              Model model,
+              @RequestParam(value="page", defaultValue="0") int page
+    )
     {
+        Page<Community> paging = communityService.list(page);
+        model.addAttribute("paging",paging);
         return "community/community";
     }
 
@@ -28,9 +39,12 @@ public class CommunityController {
     }
 
     @PostMapping("/community/editProc")
-    public String editProc()
+    public String editProc(
+            CommunityDto communityDto
+    )
     {
-        return "community/edit";
+        communityService.editProc(communityDto);
+        return "redirect:/community";
     }
 
 }
