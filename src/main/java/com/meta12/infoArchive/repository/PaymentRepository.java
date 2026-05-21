1d7d3608de74c5cbe969e9a6f60c1511977eeb35
mate12 <cnjzzan@naver.com>
2026-05-21 16:14:04 +0900

    Jeong Won

--- src/main/java/com/meta12/infoArchive/repository/PaymentRepository.java
+++ src/main/java/com/meta12/infoArchive/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.meta12.infoArchive.repository;
 
 import com.meta12.infoArchive.entity.Payment;
+import com.meta12.infoArchive.entity.PaymentStatus;
 import com.meta12.infoArchive.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -23,4 +24,11 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     long countByUserAndProductIsNull(User user);
 
     List<Payment> findTop5ByUserOrderByOrderDateDesc(User user);
+
+    // 결제 완료한 강의인지 확인
+    boolean existsByUserAndProductIdAndPaymentStatus(
+            User user,
+            Long productId,
+            PaymentStatus paymentStatus
+    );
 }
