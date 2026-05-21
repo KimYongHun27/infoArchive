21d12df6c3e3549f3c8546cb07a3a5e9a2a6c407
mate12 <cnjzzan@naver.com>
2026-05-21 09:57:36 +0900

    Jeong Won

--- src/main/java/com/meta12/infoArchive/config/WebConfig.java
+++ src/main/java/com/meta12/infoArchive/config/WebConfig.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+        Path uploadPath = Paths.get(System.getProperty("user.dir"), "uploads");
+        String uploadAbsolutePath = uploadPath.toFile().getAbsolutePath();
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations("file:" + uploadAbsolutePath + "/");
     }
 }
